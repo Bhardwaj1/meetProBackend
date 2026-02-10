12544a130535e19fc2b09552915a3e3a58d62f36
Bhardwaj1 <bhardwajgaurav204@gmail.com>
2026-02-10 13:48:16 +0530

    .

--- src/controllers/meetingHistory.controller.js
+++ src/controllers/meetingHistory.controller.js
@@ -1,5 +1,28 @@
+const asyncHandler = require("express-async-handler");
+const MeetingHistory = require("../models/MeetingHistory");
+const getMeetingsHistory = asyncHandler(async (req, res) => {
+  const userId = req.user._id;
+  const { page = 1, limit = 20 } = req.query();
 
-const getMeetingsHistory=async(req,res)=>{
-    
-};
-module.exports={getMeetingsHistory}
+  const skip = (page - 1) * limit;
+
+  const [records, total] = await Promise.all([
+    MeetingHistory.find({ user: userId })
+      .sort({ joinedAt: -1 })
+      .skip(skip)
+      .limit(Number(limit))
+      .lean(),
+    MeetingHistory.countDocuments({ user: userId }),
+  ]);
+
+  res.json({
+    success: true,
+    data: records,
+    meta: {
+      total,
+      page: Number(page),
+      limit: Number(limit),
+    },
+  });
+});
+module.exports = { getMeetingsHistory };
