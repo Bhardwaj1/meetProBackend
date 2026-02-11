16ea36fdbfa1254d1cd9a0fff65566af2933d27a
Bhardwaj1 <bhardwajgaurav204@gmail.com>
2026-02-11 13:48:10 +0530

    Export meeting api done

--- src/controllers/meetingHistory.controller.js
+++ src/controllers/meetingHistory.controller.js
@@ -1,4 +1,5 @@
 const asyncHandler = require("express-async-handler");
+const Exceljs = require("exceljs");
 const MeetingHistory = require("../models/MeetingHistory");
 const getMeetingsHistory = asyncHandler(async (req, res) => {
   const userId = req.user._id;
@@ -10,7 +11,8 @@ const getMeetingsHistory = asyncHandler(async (req, res) => {
     MeetingHistory.find({ user: userId })
       .sort({ joinedAt: -1 })
       .skip(skip)
-      .limit(Number(limit)).populate("user","name email")
+      .limit(Number(limit))
+      .populate("user", "name email")
       .lean(),
     MeetingHistory.countDocuments({ user: userId }),
   ]);
@@ -25,4 +27,62 @@ const getMeetingsHistory = asyncHandler(async (req, res) => {
     },
   });
 });
-module.exports = { getMeetingsHistory };
+
+const exportMeeting = asyncHandler(async (req, res) => {
+  const userId = req.user._id;
+
+  console.log(req.user.name, "nameeeeeeeeeeeeeeeeeeeee");
+
+  // filter same as api list
+  const { search, fromDate, toDate, status } = req.query;
+  const filter = { user: userId };
+
+  if (status) {
+    filter.status = status;
+  }
+
+  if (fromDate || toDate) {
+    filter.joinedAt = {};
+    if (fromDate) {
+      filter.joinedAt.$gte = new Date(fromDate);
+      filter.joinedAt.$lte = new Date(toDate);
+    }
+  }
+
+  const records = await MeetingHistory.find(filter)
+    .sort({ joinedAt: -1 })
+    .populate("user", "name email")
+    .lean();
+  const workbook = new Exceljs.Workbook();
+  const worksheet = workbook.addWorksheet(`${req.user.name} Meeting History`);
+
+  worksheet.columns = [
+    { header: "User Name", key: "name", width: 25 },
+    { header: "Email", key: "email", width: 30 },
+    { header: "Status", key: "status", width: 15 },
+    { header: "Joined At", key: "joinedAt", width: 25 },
+  ];
+
+  records.forEach((item) => {
+    worksheet.addRow({
+      name: item?.user?.name,
+      email: item?.user?.email,
+      status: item?.status,
+      joinedAt: item?.joinedAt,
+    });
+  });
+  worksheet.getRow(1).font = { bold: true };
+
+  res.setHeader(
+    "Content-Type",
+    "application/vnd.vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+  );
+  res.setHeader(
+    "Content-Disposition",
+    "attachment; filename=meeting-history.xlsx",
+  );
+
+  await workbook.xlsx.write(res);
+  res.end();
+});
+module.exports = { getMeetingsHistory, exportMeeting };
--- src/routes/meetingHistory.routes.js
+++ src/routes/meetingHistory.routes.js
@@ -3,8 +3,10 @@ const router = express.Router();
 const protect = require("../middleware/authMiddleware");
 const {
   getMeetingsHistory,
+  exportMeeting
 } = require("../controllers/meetingHistory.controller");
 
 router.get("/", protect, getMeetingsHistory);
+router.get('/export',protect,exportMeeting);
 
 module.exports = router;
